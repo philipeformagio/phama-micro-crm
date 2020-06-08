38dfad4581f20500e4dac1574c4a6ee9e8e8ea71
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-06-08 15:33:38 -0300

    Initial Commit
    Initial Commit

---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Model/Entities/Company.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhamaMicroCrm.Model.Entities
+{
+    public class Company
+    {
+    }
+}
---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Model/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhamaMicroCrm.Model.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = new Guid();
+        }
+    }
+}
