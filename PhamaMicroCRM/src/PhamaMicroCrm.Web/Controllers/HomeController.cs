33f5c11c8621f63b9acd11d64e84c8535c4e0369
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-06-09 09:41:13 -0300

    Error action and view

--- PhamaMicroCRM/src/PhamaMicroCrm.Web/Controllers/HomeController.cs
+++ PhamaMicroCRM/src/PhamaMicroCrm.Web/Controllers/HomeController.cs
@@ -20,10 +20,35 @@ namespace PhamaMicroCrm.Web.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [Route("erro/{id:length(3,3)}")]
+        public IActionResult Errors(int id)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var modelError = new ErrorViewModel();
+
+            if (id == 500)
+            {
+                modelError.Mensagem = "Ocorreu um erro! Tente novamente mais tarde ou contate nosso suporte.";
+                modelError.Titulo = "Ocorreu um erro!";
+                modelError.ErrorCode = id;
+            }
+            else if (id == 404)
+            {
+                modelError.Mensagem = "A página que está procurando não existe! <br> Em caso de dúvidas entre em contato com nosso suporte";
+                modelError.Titulo = "Ops! Página não encontrada.";
+                modelError.ErrorCode = id;
+            }
+            else if (id == 403)
+            {
+                modelError.Mensagem = "Você não tem premissão para fazer isto.";
+                modelError.Titulo = "Acesso Negado";
+                modelError.ErrorCode = id;
+            }
+            else
+            {
+                return StatusCode(500);
+            }
+
+            return View("Error", modelError);
         }
     }
 }
--- PhamaMicroCRM/src/PhamaMicroCrm.Web/Models/ErrorViewModel.cs
+++ PhamaMicroCRM/src/PhamaMicroCrm.Web/Models/ErrorViewModel.cs
@@ -1,11 +1,9 @@
-using System;
-
 namespace PhamaMicroCrm.Web.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string Mensagem { get; set; }
+        public string Titulo { get; set; }
+        public int ErrorCode { get; set; }
     }
 }
