526c714c733b6ce73e610e27fdfb7ce31ee787c8
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-06-10 12:50:53 -0300

    Create IRepository.cs
    Generic Repository

---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Data/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using PhamaMicroCrm.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PhamaMicroCrm.Data.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Add(TEntity entity);
+
+        Task<TEntity> GetById(Guid id);
+
+        Task<List<TEntity>> GetAll();
+
+        Task Updade(TEntity entity);
+
+        Task Remove(Guid id);
+
+        Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> predicate);
+
+        Task<int> SaveChanges();
+    }
+}
