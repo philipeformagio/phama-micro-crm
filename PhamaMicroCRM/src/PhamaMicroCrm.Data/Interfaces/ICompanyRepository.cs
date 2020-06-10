1fc38aecd2787e4e8221c2f1c7c79645b0ef7c19
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-06-10 12:55:36 -0300

    Update ICompanyRepository.cs

--- PhamaMicroCRM/src/PhamaMicroCrm.Data/Interfaces/ICompanyRepository.cs
+++ PhamaMicroCRM/src/PhamaMicroCrm.Data/Interfaces/ICompanyRepository.cs
@@ -1,8 +1,11 @@
 ﻿using PhamaMicroCrm.Model.Entities;
+using System;
+using System.Threading.Tasks;
 
 namespace PhamaMicroCrm.Data.Interfaces
 {
     public interface ICompanyRepository : IRepository<Company>
     {
+        Task<Company> GetCompanyWithUnits(Guid companyId);
     }
 }
