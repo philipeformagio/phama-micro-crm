fc354f6bf9c5843ca43842ad2df4a5b88b93115b
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-06-10 12:54:40 -0300

    Create ICompanyUnitRepository.cs

---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Data/Interfaces/ICompanyUnitRepository.cs
@@ -0,0 +1,11 @@
+﻿using PhamaMicroCrm.Model.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace PhamaMicroCrm.Data.Interfaces
+{
+    public interface ICompanyUnitRepository : IRepository<CompanyUnit>
+    {
+        Task<CompanyUnit> GetCompanyUnitWithAddress(Guid companyUnitId);
+    }
+}
