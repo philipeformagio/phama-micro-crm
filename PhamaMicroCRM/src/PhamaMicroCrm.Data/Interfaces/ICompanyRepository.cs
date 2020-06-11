47267089a4cac5fb9950fd499e280a54a99bff38
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-06-11 18:11:34 -0300

    CompanyRepo

--- PhamaMicroCRM/src/PhamaMicroCrm.Data/Interfaces/ICompanyRepository.cs
+++ PhamaMicroCRM/src/PhamaMicroCrm.Data/Interfaces/ICompanyRepository.cs
@@ -1,5 +1,6 @@
 ﻿using PhamaMicroCrm.Model.Entities;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PhamaMicroCrm.Data.Interfaces
@@ -7,5 +8,7 @@ namespace PhamaMicroCrm.Data.Interfaces
     public interface ICompanyRepository : IRepository<Company>
     {
         Task<Company> GetCompanyWithUnits(Guid companyId);
+
+        Task<IEnumerable<Company>> GetCompaniesWithUnits();
     }
 }
---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Data/Repository/CompanyRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhamaMicroCrm.Data.Interfaces;
+using PhamaMicroCrm.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhamaMicroCrm.Data.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        public async Task<Company> GetCompanyWithUnits(Guid companyId)
+        {
+            return await Db.Companies.AsNoTracking()
+                           .Include(c => c.CompanyUnits)
+                           .FirstOrDefaultAsync(c => c.Id == companyId);
+        }
+
+        public async Task<IEnumerable<Company>> GetCompaniesWithUnits()
+        {
+            return await Db.Companies.AsNoTracking()
+                           .Include(c => c.CompanyUnits)
+                           .ToListAsync();
+        }
+    }
+}
