5f42d8f6d6a964a01593a4e5e4971140fcbb8190
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-06-11 17:42:03 -0300

    Reposity concrete class created.

---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Data/Repository/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhamaMicroCrm.Data.Context;
+using PhamaMicroCrm.Data.Interfaces;
+using PhamaMicroCrm.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhamaMicroCrm.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        protected readonly PhamaMicroCrmContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+
+        public Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Updade(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
