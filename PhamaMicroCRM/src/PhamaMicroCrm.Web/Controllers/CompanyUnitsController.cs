dc511a4064f74cb6d1f7b068cee7ecc3b3ae8279
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-06-13 21:43:50 -0300

    CompanyUnit

---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Web/Controllers/CompanyUnitsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PhamaMicroCrm.Business.Interfaces;
+
+namespace PhamaMicroCrm.Web.Controllers
+{
+    public class CompanyUnitsController : BaseController
+    {
+        public CompanyUnitsController(INotifier notifier) : base(notifier)
+        {
+
+        }
+    }
+}
