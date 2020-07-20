bfd4f7378648938d9bdd9d50d688acb86ab387be
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-07-19 23:16:39 -0300

    taghelpers

--- PhamaMicroCRM/src/PhamaMicroCrm.Web/TagHelpers/TelephoneTagHelper.cs
+++ PhamaMicroCRM/src/PhamaMicroCrm.Web/TagHelpers/TelephoneTagHelper.cs
@@ -15,24 +15,45 @@ namespace PhamaMicroCrm.Web.TagHelpers
             var content = await output.GetChildContentAsync();
 
             System.Text.StringBuilder builder = new System.Text.StringBuilder();
-            builder.Append("(" + content.GetContent().Substring(0, 2) + ") ");
 
-            if (content.GetContent().Substring(2, 1) == "9")
+            if (content.GetContent().Count() == 8 || content.GetContent().Count() == 9)
             {
-                builder.Append(content.GetContent().Substring(2, 5));
-                builder.Append("-");
-                builder.Append(content.GetContent().Substring(7, 4));
+                if (content.GetContent().Substring(0, 1) == "9")
+                {
+                    builder.Append(content.GetContent().Substring(0, 4));
+                    builder.Append("-");
+                    builder.Append(content.GetContent().Substring(5, 4));
+                }
+                else
+                {
+                    builder.Append(content.GetContent().Substring(0, 4));
+                    builder.Append("-");
+                    builder.Append(content.GetContent().Substring(4, 4));
+                }
+
+                var target = builder.ToString();
+                output.Content.SetContent(target);
             }
             else
             {
-                builder.Append(content.GetContent().Substring(2, 4));
-                builder.Append("-");
-                builder.Append(content.GetContent().Substring(6, 4));
+                builder.Append("(" + content.GetContent().Substring(0, 2) + ") ");
+
+                if (content.GetContent().Substring(2, 1) == "9")
+                {
+                    builder.Append(content.GetContent().Substring(2, 5));
+                    builder.Append("-");
+                    builder.Append(content.GetContent().Substring(7, 4));
+                }
+                else
+                {
+                    builder.Append(content.GetContent().Substring(2, 4));
+                    builder.Append("-");
+                    builder.Append(content.GetContent().Substring(6, 4));
+                }
+
+                var target = builder.ToString();
+                output.Content.SetContent(target);
             }
-
-            var target = builder.ToString();
-
-            output.Content.SetContent(target);
         }
     }
 }
