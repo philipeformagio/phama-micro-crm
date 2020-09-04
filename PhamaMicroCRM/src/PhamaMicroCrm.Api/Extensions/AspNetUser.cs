09c9f65ae74c802d93cb6fe1ad221a17d4596f92
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-09-04 16:30:00 -0300

    Update AspNetUser.cs

--- PhamaMicroCRM/src/PhamaMicroCrm.Api/Extensions/AspNetUser.cs
+++ PhamaMicroCRM/src/PhamaMicroCrm.Api/Extensions/AspNetUser.cs
@@ -1,11 +1,72 @@
-﻿using System;
+﻿/*
+ * this class is a representation of a logged user
+ */
+using Microsoft.AspNetCore.Http;
+using PhamaMicroCrm.Api.Interfaces;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PhamaMicroCrm.Api.Extensions
 {
-    public class AspNetUser
+    public class AspNetUser : IUser
     {
+        private readonly IHttpContextAccessor _accessor;
+        public AspNetUser(IHttpContextAccessor accessor)
+        {
+            this._accessor = accessor;
+        }
+
+        public string Name => _accessor.HttpContext.User.Identity.Name;
+
+        public Guid GetUserId()
+        {
+            return IsAuthenticated() ? Guid.Parse(_accessor.HttpContext.User.GetUserId()) : Guid.Empty;
+        }
+
+        public string GetUserEmail()
+        {
+            return IsAuthenticated() ? _accessor.HttpContext.User.GetUserEmail() : "";
+        }
+
+        public IEnumerable<Claim> GetClaimsIdentity()
+        {
+            return _accessor.HttpContext.User.Claims;
+        }
+
+        public bool IsInRole(string role)
+        {
+            return _accessor.HttpContext.User.IsInRole(role);
+        }
+
+        public bool IsAuthenticated()
+        {
+            return _accessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+    }
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+
+            var claim = principal.FindFirst(ClaimTypes.NameIdentifier);
+            return claim?.Value;
+        }
+
+        public static string GetUserEmail(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+
+            var claim = principal.FindFirst(ClaimTypes.Email);
+            return claim?.Value;
+        }
     }
 }
