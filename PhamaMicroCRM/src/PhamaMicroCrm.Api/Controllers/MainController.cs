14195b8269fbce3af0955860503be17fbf506c25
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-09-04 16:22:41 -0300

    api project created

---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Api/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhamaMicroCrm.Business.Interfaces;
+
+namespace PhamaMicroCrm.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        private readonly INotifier _notifier;
+        public MainController(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+    }
+}
