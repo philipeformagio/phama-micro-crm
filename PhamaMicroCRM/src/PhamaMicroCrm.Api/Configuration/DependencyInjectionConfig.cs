e0b62564c7636211727aed0e968989bcfcf9d1d9
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-09-04 16:25:36 -0300

    Create DependencyInjectionConfig.cs

---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PhamaMicroCrm.Business.Interfaces;
+using PhamaMicroCrm.Business.Notifications;
+using PhamaMicroCrm.Business.Services;
+using PhamaMicroCrm.Data.Context;
+using PhamaMicroCrm.Data.Interfaces;
+using PhamaMicroCrm.Data.Repository;
+
+namespace PhamaMicroCrm.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<PhamaMicroCrmContext>();
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
+            services.AddScoped<ICompanyUnitRepository, CompanyUnitRepository>();
+            services.AddScoped<IContactRepository, ContactRepository>();
+            services.AddScoped<INoteRepository, NoteRepository>();
+
+            services.AddScoped<INotifier, Notifier>();
+            services.AddScoped<ICompanyService, CompanyService>();
+            services.AddScoped<ICompanyUnitService, CompanyUnitService>();
+            services.AddScoped<IContactService, ContactService>();
+            services.AddScoped<INoteService, NoteService>();
+
+            return services;
+        }
+
+    }
+}
