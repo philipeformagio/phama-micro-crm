5345177594930d4a0fbf45ef0acdfb58bdfb32a7
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-11-10 08:47:52 -0300

    Testing JS

---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Web/wwwroot/js/Entities/Company.js
@@ -0,0 +1,13 @@
+﻿class Company {
+    constructor(name, field, active, phone) {
+        this.name = name;
+        this.field = field;
+        this.active = active;
+        this.phone = phone;
+    }
+
+    //methods
+    greet() {
+        console.log(`Hi, I'm a company called ${this.name}`);
+    }   
+}
