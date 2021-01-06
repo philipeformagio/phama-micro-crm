b12a2d1e4fc0d3ace7c3b70a0ad42ff417b186fb
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2021-01-06 10:28:22 -0300

    EmailBusinessTagHelper created and implemented.

---
+++ PhamaMicroCRM/src/PhamaMicroCrm.Web/TagHelpers/EmailBusinessTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace PhamaMicroCrm.Web.TagHelpers
+{
+    public class EmailBusinessTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            //<email-business>philipe</email-business>
+            // => <a href="mailto:philipe.formagio@gmail.com">philipe.formagio@gmail.com</a>
+
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + ".formagio@" + "gmail.com";
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
